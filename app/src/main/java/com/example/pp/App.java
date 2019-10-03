8241bf55ede56a08534a4a5f6ee7ab2ba58340b6
slava <slavakuzmuk1986@gmail.com>
2019-10-03 09:48:34 +0300

    cresh при додаванні тестових записів в БД

--- app/src/main/java/com/example/pp/App.java
+++ app/src/main/java/com/example/pp/App.java
@@ -5,6 +5,7 @@ import android.app.Application;
 public class App extends Application {
     @Override
     public void onCreate() {
+
         super.onCreate();
 
     }
--- app/src/main/java/com/example/pp/ShopAdapter.java
+++ app/src/main/java/com/example/pp/ShopAdapter.java
@@ -8,6 +8,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class ShopAdapter extends RecyclerView.Adapter<ShopAdapter.ShopViewHolder> {
     private Context mContext;
